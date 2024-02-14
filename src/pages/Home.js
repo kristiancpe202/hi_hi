1686ac045e097733e8f7db881fa390a0cd01d89c
kristian delossantos <Delossantoskristian5@gmail.com>
2024-02-14 19:47:23 +0800

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import Nav from "../components/Nav"
+import AuthModal from "../components/AuthModal"
+import { useState } from 'react'
+
+
+const Home = () => {
+    const [showModal, setShowModal] = useState(false)
+    
+    const [isSignup, setIsSignUp] = useState(true)
+
+    const authToken = false
+
+    const handleClick = () => {
+        console.log('clicked')
+        setShowModal(true)
+        setIsSignUp(true)
+    }
+    return (
+
+        <div className="overlay">
+            <Nav minimal={false} 
+           
+            setShowModal={setShowModal} 
+            showModal={showModal}
+            setIsSignUp={setIsSignUp}
+            />
+            <div className="home">
+                <h1 className="primary-title">Say HI!</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignup}/>
+                )}
+            </div>
+        </div>
+
+    )
+}
+export default Home
